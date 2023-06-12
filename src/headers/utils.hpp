0a2174d4e441e2b0d3714ec63ecf2c4398503aa3
BPhilagor <benjamin.philagor@gmail.com>
2023-06-12 14:16:32 +0200

    Commit

--- src/headers/utils.hpp
+++ src/headers/utils.hpp
@@ -6,15 +6,13 @@
 /*   By: bphilago <bphilago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/11 15:24:53 by bphilago          #+#    #+#             */
-/*   Updated: 2023/06/11 15:24:53 by bphilago         ###   ########.fr       */
+/*   Updated: 2023/06/12 13:09:17 by bphilago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTILS_HPP
 # define UTILS_HPP
 # include <sys/socket.h>
-//#include <sys/epoll.h> pour Linux
-//#include <sys/event.h> pour MacOS
 # include <unistd.h>
 # include <stdio.h>
 # include <string.h>
@@ -23,6 +21,8 @@
 # include <netinet/in.h>
 # include <sstream>
 # include <string>
+# include <vector>
+# include <unordered_map>
 
 int send_to_socket(const std::string &message, int socket_fd);
 int send_file_to_socket(const std::string &filename, int socket_fd);
