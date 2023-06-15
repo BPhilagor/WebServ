821d12511682de380fbe5e6ec878d7d87dd18464
Nicholas Zivkovic <znichola@student.42lausanne.ch>
2023-06-15 09:06:10 +0200

    map test for error codes

--- src/HTTPResponse_serverErrorResponse.cpp
+++ src/HTTPResponse_serverErrorResponse.cpp
@@ -40,3 +40,52 @@ static const std::string reason(int code)
 		return "";
 	return reason[code];
 }
+
+// would have been nicer to use a map, but it's c++ 11 to initialise like this
+// map<int,int> create_map()
+// {
+//   map<int,int> m;
+//   m[1] = 2;
+//   m[3] = 4;
+//   m[5] = 6;
+//   return m;
+// }
+// map<int,int> m = create_map();
+// this could be a solution but then we need to initialise it, and we'd want to
+// avoid doing that for every instance of the class, idk.
+
+	// static const std::map<int, std::string> reason{
+	// 	{500, "Internal Server Error"},
+	// 	{501, "Not Implemented"},
+	// 	{502, "Bad Gateway"},
+	// 	{503, "Service Unavailable"},
+	// 	{504, "Gateway Timeout"},
+	// 	{505, "HTTP Version Not Supported"},
+	// 	{506, "Variant Also Negotiates"},
+	// 	{507, "Insufficient Storage"},
+	// 	{508, "Loop Detected"},
+	// 	{510, "Not Extended"},
+	// 	{511, "Network Authentication Required"},
+	// 	{500, "Internal Server Error"},
+	// 	{501, "Not Implemented"},
+	// 	{502, "Bad Gateway"},
+	// 	{503, "Service Unavailable"},
+	// 	{504, "Gateway Timeout"},
+	// 	{505, "HTTP Version Not Supported"},
+	// 	{506, "Variant Also Negotiates"},
+	// 	{507, "Insufficient Storage"},
+	// 	{508, "Loop Detected"},
+	// 	{510, "Not Extended"},
+	// 	{511, "Network Authentication Required"},
+	// 	{500, "Internal Server Error"},
+	// 	{501, "Not Implemented"},
+	// 	{502, "Bad Gateway"},
+	// 	{503, "Service Unavailable"},
+	// 	{504, "Gateway Timeout"},
+	// 	{505, "HTTP Version Not Supported"},
+	// 	{506, "Variant Also Negotiates"},
+	// 	{507, "Insufficient Storage"},
+	// 	{508, "Loop Detected"},
+	// 	{510, "Not Extended"},
+	// 	{511, "Network Authentication Required"}
+	// };
