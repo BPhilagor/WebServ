9a43c67279751ad8331649b3384e026e67f1d8cb
Sophia Koulen <sophia.koulen@outlook.com>
2023-08-02 15:34:10 +0200

    /php-no and /php-yes folders in order to show cgi config

--- websites/basic_site/php-yes/index.php
+++ websites/basic_site/php-yes/index.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>tst.php</title>
+	<title>Testing PHP</title>
 </head>
 <body>
 	<h1>A file with PHP code</h1>
