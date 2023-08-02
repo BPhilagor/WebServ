411e2f7ddd2498e331df56f35112488d2d0bda71
Sophia Koulen <sophia.koulen@outlook.com>
2023-08-02 15:17:54 +0200

    made tst.php more explicit

--- websites/basic_site/tst.php
+++ websites/basic_site/tst.php
@@ -1,4 +1,14 @@
-<?php
-echo "Hello, World!";
-?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>tst.php</title>
+</head>
+<body>
+	<h1>A file with PHP code</h1>
+	<p>This should output "Hello, world!" <b>if PHP is enabled<b><p>
+	<?php echo "Hello, World!"; ?>
+</body>
+</html>
